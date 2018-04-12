7d08eaac72c1b037cb9c9a866faa6a12188ffba4
Angel Ruiz <angel@cultureamp.com>
2018-04-12 13:43:55 +1000

    Create a few types

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 // @flow
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import foxesData from "./data/foxes.json";
 
-class App extends Component<{}, {}> {
+type Props = { data: FoxData };
+
+type FoxData = {
+  posts: Array<Post>,
+  users: Array<User>
+};
+
+type Post = {
+  id: string,
+  userId: string,
+  imageUrl: string,
+  description: string
+};
+
+type User = {
+  id: string,
+  username: string
+};
+
+class App extends Component<Props, {}> {
+  constructor(props: Props) {
+    super(props);
+  }
   render() {
-    return <div>Hello World!</div>;
+    return <div>{this.props.data.posts[0].description}</div>;
   }
 }
 
 const appRoot = document.getElementById("root");
-appRoot && ReactDOM.render(<App />, appRoot);
+appRoot && ReactDOM.render(<App data={foxesData} />, appRoot);
